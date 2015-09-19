1f4fa8dc6e3082f78d8b7bb5b4994a297fe60097
tguglanaklona <tguglanaklona@gmail.com>
2015-09-19 23:47:00 +0300

    m

--- hello.js
+++ hello.js
@@ -1,30 +1,25 @@
 console.log('Hello, world!');
 var page = require('webpage').create();
-<<<<<<< HEAD
 	console.log("space");
-page.open('http://www.habrahabr.ru', function(status) {
-=======
-console.log("h1");
-page.open('http://www.babyblog.ru', function(status) {
->>>>>>> 192d3454679aa20a3352ad594a2187fd421fa99c
+page.open('http://www.google.com', function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
 		console.log('success');
 		page.render('./example.png');
 	}
+	phantom.exit();
 });
 page.onError = function(msg, trace){
-	console.log('error');
 	var msgStack = ['ERROR '+msg];
 
 	if (trace && trace.length) {
 		msgStack.push('TRACE:');
 		trace.forEach(function(t) {
 			msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
-		});
-	}
+});
+}
 
-	console.error(msgStack.join('\n'));
-	console.log(msgStack.join('\n'));
+console.error(msgStack.join('\n'));
+
 };
 phantom.exit();
